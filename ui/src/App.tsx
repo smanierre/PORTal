ac603159d7551e90330da4c4d71c77bc58a6bdbf
Sean Manierre <sean.manierre@gmail.com>
2024-07-09 10:15:18 -0400

    Added in base react project with shadcn

---
+++ ui/src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import {Button} from "./components/ui/button.tsx";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return <h1>App</h1>
+}
+
+export default App
