c2407b30ee7296112424f65a47d37410bad7c43e
Sean Manierre <sean.manierre@gmail.com>
2025-02-28 10:00:05 -0500

    Added in node for separating out web components

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 go install github.com/air-verse/air@latest
 go install github.com/a-h/templ/cmd/templ@latest
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo bash -
+sudo apt-get install -y nodejs
+
